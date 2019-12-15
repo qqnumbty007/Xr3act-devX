63f89f74e24288b1aae53c3f0c306e5746f6f189
Thanapon Joosakul <qqnumbty@hotmail.com>
2019-12-15 16:21:52 +0700

    add reducers on Product, Order to index

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+// import reducers from './reducers/index';
+import reducers from './reducers'; // เหมือนกับไลน์บน
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+// เพิ่ม store บน redux  ------ * ------ มาจากเว็บของ redux ไล่ตาม step ไป
+// 1. สร้าง store ไว้เก็บ state
 
 const TestFunctionComponent = () => {
     return (
@@ -15,6 +24,10 @@ const TestFunctionComponent = () => {
     )
 } // test Function Component
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
+// Provider คือการ set store ให้กับ App
 serviceWorker.unregister();
